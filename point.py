19e172f882033959f114427c9438fadfcdc76dd9
kmkacpermajor <kmkacpermajor@gmail.com>
2022-10-23 17:25:43 +0200

    from main

---
+++ point.py
@@ -0,0 +1,10 @@
+class Point():
+    def __init__(self, pointArr):
+        self.x = pointArr[0]
+        self.y = pointArr[1]
+
+    def y(self):
+        return self.y
+
+    def x(self):
+        return self.x
