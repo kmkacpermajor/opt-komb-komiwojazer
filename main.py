a643235dc3bfd6dace78a613359713445526156b
kmkacpermajor <kmkacpermajor@gmail.com>
2022-10-24 07:00:18 +0200

    print

--- main.py
+++ main.py
@@ -10,5 +10,8 @@ arrPoints = generatePointsArray(26,[-100,100],[-1000,1000])
 # arrPoints = readPointsFile("examples/ekursy.txt")
 # generateFile(arrPoints, "przyklad2.txt")
 
+for x in arrPoints:
+    print(x.x, x.y)
+
 resTSP = findTSPResolution(arrPoints)
 print('TSP resolution is {}'.format(resTSP))
