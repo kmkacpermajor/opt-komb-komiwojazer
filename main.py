77f626e698135d189c33bf5644a503838625f6f2
Student 151753 <inf151753@lab-dbx-11.cs.put.poznan.pl>
2022-11-21 12:48:57 +0100

    laby

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from generator import *
 from calculator import *
-
-# zrób do tego jakieś menu proste:
-# czy odczytać
-# czy zapisać
-# ile punktów i jakie przedziały
+from greedy import *
 
 arrPoints = generatePointsArray(26,[-100,100],[-1000,1000])
 # arrPoints = readPointsFile("examples/ekursy.txt")
 # generateFile(arrPoints, "przyklad2.txt")
+
+# warunki stopu
+# czas: max 5 minut
+# pliki: berlin52, bier127, tsp250, tsp500, tsp1000
 
 for x in arrPoints:
     print(x.x, x.y)
